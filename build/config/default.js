32779696321f9b1ae3863532fbaed500d46aac7a
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-01-31 10:42:36 +0530

    removde build folder

--- build/config/default.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: 8080,
-    dbUri: "mongodb+srv://muntha:xdaZoUHqUoi2NQi7@cluster0.rgttnh6.mongodb.net/?retryWrites=true&w=majority",
-};
